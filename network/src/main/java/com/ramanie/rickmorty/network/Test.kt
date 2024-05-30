aecf7cb1922589c7115ba8fe85860732829846bc
kenjr <kenedy@myhechar.pro>
2024-05-30 20:04:07 +0300

    compose test file

---
+++ network/src/main/java/com/ramanie/rickmorty/network/Test.kt
@@ -0,0 +1,9 @@
+package com.ramanie.rickmorty.network
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestText(){
+    Text(text = "Hello from the networking module :)")
+}
